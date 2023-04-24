a22ff7124f5576969bc054c27fd5094394260fc4
Mahmood Amintoosi <mamintoosi@users.noreply.github.com>
2023-04-24 20:19:47 +0330

    Update all

---
+++ copy_to_other_reps.bat
@@ -0,0 +1,24 @@
+mkdocs build
+@REM git commit -am "Auto Commit"
+@REM git push
+
+cd ..
+cd curr
+xcopy ..\mkdocs-base\*.md .
+cd docs
+xcopy ..\..\mkdocs-base\docs\base base
+xcopy ..\..\mkdocs-base\docs\elective elective
+xcopy ..\..\mkdocs-base\docs\mandatory mandatory
+@REM git commit -am "Auto Commit"
+@REM git push
+
+cd ..
+cd ..
+cd cs-um.github.io\docs
+xcopy ..\..\mkdocs-base\site . /e
+@REM git commit -am "Auto Commit"
+@REM git push
+
+cd ..
+cd ..
+cd mkdocs-base
