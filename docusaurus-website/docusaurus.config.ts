44659d219de166c609fa5e9588707a8ab75678d4
Seyed Masih Sajadi <smasihsajadi@gmail.com>
2024-01-18 22:09:04 +0330

    feat: update font to Vazirmatn

--- docusaurus-website/docusaurus.config.ts
+++ docusaurus-website/docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: ['./src/css/custom.css', './src/css/font.css'],
         },
       } satisfies Preset.Options,
     ],
