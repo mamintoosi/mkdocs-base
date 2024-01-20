5b1a258e61531b7dc3c6ffb9d3569ea9917f4b35
Mahmood Amintoosi <mamintoosi@users.noreply.github.com>
2024-01-20 23:11:54 +0330

    Edit docusaurus.config.ts

--- docusaurus-website/docusaurus.config.ts
+++ docusaurus-website/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://fumcs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
